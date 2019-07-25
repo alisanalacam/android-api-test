3d5549c6626210818568aeafeb2a4e0617468b3e
Alisan ALACAM <alisan.alacam@wtd-tech.com>
2019-07-25 10:56:09 +0300

    first commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UsersResource;
+
+use App\User;
+use Illuminate\Http\Request;
+/**
+ * Class UsersController
+ * @property User user
+ * @package App\Http\Controllers
+ */
+class UsersController extends Controller
+{
+    /**
+     * @var User
+     */
+    protected $user;
+    /**
+     * UsersController constructor.
+     *
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+    /**
+     * List of users in Json format
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index(Request $request)
+    {
+        $query = $this->user->orderBy($request->column, $request->order);
+        $users = $query->paginate($request->per_page ?? 10);
+        return UsersResource::collection($users);
+    }
+}
---
+++ app/Http/Resources/UsersResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UsersResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'        =>  $this->id,
+            'name'       => $this->name,
+            'email'      => $this->email,
+            'address'    => $this->address,
+            'created_at' => Carbon::parse($this->created_at)->toDayDateTimeString(),
+        ];
+    }
+}
