3f9832dd0f197261ecdd2e0aad9d918eb9f22ded
Alisan ALACAM <alisan.alacam@wtd-tech.com>
2019-07-25 11:03:03 +0300

    user api

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('users', 'UsersController@index')->name('users');
+Route::post('users', 'Auth\RegisterController@create')->name('users');
